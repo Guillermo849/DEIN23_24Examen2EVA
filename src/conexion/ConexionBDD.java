3911e8964dfca6d4e292f2edb02772bbc7e53984
Guillermo849 <orivealonsoguillermo@gmail.com>
2024-02-01 09:52:38 +0100

    MOD Conexion BDD para que utilice properies

--- src/conexion/ConexionBDD.java
+++ src/conexion/ConexionBDD.java
@@ -1,22 +1,34 @@
 package conexion;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /* Establece la conexion con la base de datos */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.TimeZone;
+import java.util.Properties;
 
 public class ConexionBDD {
+	private Properties prop;
     private Connection conexion;
     public ConexionBDD()  {
+    	prop = new Properties();
+		try (FileInputStream fis = new FileInputStream(
+				new File(this.getClass().getResource("/properties/properties.properties").getPath()))) {
+			prop.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     	try {
-    		String baseDatos = "paises";
-            String host = "localhost";
-            String usuario = "user3";
-            String password = "user3";
-            String cadenaConexion = "jdbc:mysql://" + host + "/" + baseDatos+ "?serverTimezone=" + TimeZone.getDefault().getID();
-            conexion = DriverManager.getConnection(cadenaConexion, usuario, password);
+    		String host = prop.getProperty("db.url");
+			String baseDatos = prop.getProperty("db.name");
+			String usuario = prop.getProperty("db.user");
+			String password = prop.getProperty("db.pwd");
+			String cadenaConexion = "jdbc:mysql://" + host + ":3306/" + baseDatos;
+			conexion = DriverManager.getConnection(cadenaConexion, usuario, password);
             conexion.setAutoCommit(true);
     	} catch (SQLException e) {
     		e.printStackTrace();
